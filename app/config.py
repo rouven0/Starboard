b72a317552d4982b7dc286f6a14619db9bf75552
Rouven <seifertrouven@gmail.com>
2022-06-26 14:24:08 +0200

    updated the embed color

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 from os import getenv
 
 LOG_FORMAT = "%(levelname)s [%(module)s.%(funcName)s]: %(message)s"
-EMBED_COLOR = int("0xFFFF00", 16)
+EMBED_COLOR = int("0x2f3136", 16)
 BASE_URL = "https://discord.com/api/v10/webhooks"
 DATABASE_ARGS = {
     "host": getenv("MYSQL_HOST"),
